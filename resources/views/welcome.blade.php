f243d666655026ef112cd7dd37b8d59ece6567d3
princewillchidi <127307527+princewillchidi@users.noreply.github.com>
2024-01-10 19:03:50 +0000

    blog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     @section('hero')
     <div class="w-full text-center py-32">
         <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
-            {{ __('welcome.hero.title') }} <span class="text-yellow-500">&lt;YELO&gt;</span> <span class="text-gray-900"> News</span>
+            {{ __('welcome.hero.title') }} <span class="text-yellow-500">&lt;PRINCE&gt;</span> <span class="text-gray-900"> News</span>
         </h1>
         <p class="text-gray-500 text-lg mt-1">{{ __('welcome.hero.desc') }}</p>
         <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
